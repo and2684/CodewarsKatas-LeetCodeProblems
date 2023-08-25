dbed91461304aad0540f86182d0433d04b600f0a
and2684 <aaaa6226@gmail.com>
2023-08-25 08:54:12 +0500

    Trying git bash instead of gui

--- Katas/LeetCode.cs
+++ Katas/LeetCode.cs
@@ -207,9 +207,19 @@ namespace hw
             }
 
             return res;
+        } 
+    }
+
+    public static class SolutionMerge
+    {
+        public static void Merge(int[] nums1, int m, int[] nums2, int n)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                nums1[m + i] = nums2[i];
+            }
+            Array.Sort(nums1);
         }
     }
     #endregion
-
-
 }
