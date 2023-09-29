c65131716e92c4c7d5d426906f0fd484cdaa9450
and2684 <aaaa6226@gmail.com>
2023-09-30 01:18:41 +0500

    Dynamic programming is hard :D

--- Katas/LeetCode.cs
+++ Katas/LeetCode.cs
@@ -1177,6 +1177,24 @@ namespace hw
         }
     }
 
+    public static class SolutionClimbStairs
+    {
+        public static int ClimbStairs(int n)
+        {
+            var dp = new int[n + 1];
+
+            dp[0] = 1;
+            dp[1] = 2;
+
+            for (int i = 2; i < n; i++)
+            {
+                dp[i] = dp[i - 1] + dp[i - 2];
+            }
+
+            return dp[n - 1];
+        }
+    }
+
 }
 
 #endregion
