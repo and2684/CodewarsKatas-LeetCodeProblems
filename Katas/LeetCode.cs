8362944916025e12ee3df33a63784c6985fce3bb
and2684 <aaaa6226@gmail.com>
2023-10-03 18:21:53 +0500

    Daily task 03.10.2023

--- Katas/LeetCode.cs
+++ Katas/LeetCode.cs
@@ -1209,6 +1209,19 @@ public class Solution
 
         return sb.ToString();
     }
+
+    public int NumIdenticalPairs(int[] nums)
+    {
+        var cnt = 0;
+        for (int i = 0; i < nums.Length - 1; i++)
+        {
+            for (int j = i + 1; j < nums.Length; j++)
+            {
+                if (nums[i] == nums[j]) cnt++;
+            }
+        }
+        return cnt;
+    }
 }
 
 #endregion
